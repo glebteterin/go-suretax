dc01da4286d567a31053f4529470601871d08993
Gleb Teterin <gleb.teterin@gmail.com>
2018-02-23 14:06:23 +1300

    fix: fix request ItemMessages mapping

--- client.go
+++ client.go
@@ -188,49 +188,49 @@ type Request struct {
 
 type RequestItem struct {
 	// Used to identify an item within the request. If no value is provided, requests are numbered sequentially. Max Len: 40
-	LineNumber             string
+	LineNumber string
 
 	// Used for tax aggregation by Invoice. Must be alphanumeric. Max Len: 40
-	InvoiceNumber          string
+	InvoiceNumber string
 
 	// Used for tax aggregation by Customer. Must be alphanumeric. Max Len: 40
-	CustomerNumber         string
+	CustomerNumber string
 
 	// Required when using Tax Situs Rule 01 or 03. Format: NPANXXNNNN
-	OrigNumber   string
+	OrigNumber string
 
 	// Required when using Tax Situs Rule 01. Format: NPANXXNNNN
-	TermNumber   string
+	TermNumber string
 
 	// Required when using Tax Situs Rule 01 or 02. Format: NPANXXNNNN
 	BillToNumber string
 
 	// Required. Date of transaction. Valid date formats include: MM/DD/YYYY, MM-DD-YYYY, YYYY-MM-DDTHH:MM:SS
-	TransDate              string
+	TransDate string
 
 	// Optional. Billing Period Start Date
 	BillingPeriodStartDate string
 
 	// Optional. Billing Period End Date
-	BillingPeriodEndDate   string
+	BillingPeriodEndDate string
 
 	// Required. Format: $$$$$$$$$.CCCC
 	// For Negative charges, the first position should have a minus (-) indicator.
-	Revenue                string
+	Revenue string
 
 	// Required. Values:
 	// 0 – Default (No Tax Included) 1 – Tax Included in Revenue
-	TaxIncludedCode        string
+	TaxIncludedCode string
 
 	// Required.
 	// Units representing number of “lines” or unique charges contained within the revenue.
 	// This value is essentially a multiplier on unit-based fees (e.g. E911 fees).
 	// Format: 99999. Default should be 1 (one unit).
-	Units                  string
+	Units string
 
 	// Required.
 	// 00 – Default / Number of unique access lines. *See Appendix F for additional values.
-	UnitType               string
+	UnitType string
 
 	// Required. Values:
 	// 01 – Two-out-of-Three test using NPA-NXX
@@ -243,91 +243,91 @@ type RequestItem struct {
 	// 14 – Use Zip code field for international country code (VAT calculations)
 	// 17 - Point to Point Zip codes (private line transactions) with both A and Z endpoints calculated*
 	// 27 – Use only Billing Address / Zip+4
-	TaxSitusRule           string
+	TaxSitusRule string
 
 	// Required. Transaction Type Indicator.
-	TransTypeCode          string
+	TransTypeCode string
 
 	// Required. Values:
 	// R – Residential customer (default) B – Business customer
 	// I – Industrial customer
 	// L – Lifeline customer
-	SalesTypeCode          string
+	SalesTypeCode string
 
 	// Required. Provider Type.
-	RegulatoryCode         string
+	RegulatoryCode string
 
 	// Required. Tax Exemption to be applied to this item only.
-	TaxExemptionCodeList   []string
+	TaxExemptionCodeList []string
 
 	// Required. Tax Exemption reason value.
-	ExemptReasonCode       string
+	ExemptReasonCode string
 
 	// Optional. Field for client use at the item level. This value is not returned in the response, but is available for use in reports and extracts.
 	// Max Len: 100
-	UDF                    string
+	UDF string
 
 	// Optional. Field for client use at the item level. This value is not returned in the response, but is available for use in reports and extracts.
 	// Max Len: 100
-	UDF2                   string
+	UDF2 string
 
 	// Optional. Available for use in the rules engine.
-	CostCenter             string
+	CostCenter string
 
 	// Optional. Available for use in the rules engine. Max Len: 25 Alphanumeric
-	GLAccount              string
+	GLAccount string
 
 	// Optional. Available for use in the rules engine. Max Len: 25 Alphanumeric
-	MaterialGroup          string
+	MaterialGroup string
 
 	// Optional. Billing Days in Period.
-	BillingDaysInPeriod    string
+	BillingDaysInPeriod string
 
 	// Optional. Origin Country Code
-	OriginCountryCode      string
+	OriginCountryCode string
 
 	// Optional. Destination Country Code
-	DestCountryCode        string
+	DestCountryCode string
 
 	// Optional, user defined field. Available for use in the rules engine.
 	// Max Len: 25 Alphanumeric
-	Parameter1             string
+	Parameter1 string
 	// Optional, user defined field. Available for use in the rules engine.
 	// Max Len: 25 Alphanumeric
-	Parameter2             string
+	Parameter2 string
 	// Optional, user defined field. Available for use in the rules engine.
 	// Max Len: 25 Alphanumeric
-	Parameter3             string
+	Parameter3 string
 	// Optional, user defined field. Available for use in the rules engine.
 	// Max Len: 25 Alphanumeric
-	Parameter4             string
+	Parameter4 string
 	// Optional, user defined field. Available for use in the rules engine.
 	// Max Len: 25 Alphanumeric
-	Parameter5             string
+	Parameter5 string
 	// Optional, user defined field. Available for use in the rules engine.
 	// Max Len: 25 Alphanumeric
-	Parameter6             string
+	Parameter6 string
 	// Optional, user defined field. Available for use in the rules engine.
 	// Max Len: 25 Alphanumeric
-	Parameter7             string
+	Parameter7 string
 	// Optional, user defined field. Available for use in the rules engine.
 	// Max Len: 25 Alphanumeric
-	Parameter8             string
+	Parameter8 string
 	// Optional, user defined field. Available for use in the rules engine.
 	// Max Len: 25 Alphanumeric
-	Parameter9             string
+	Parameter9 string
 	// Optional, user defined field. Available for use in the rules engine.
 	// Max Len: 25 Alphanumeric
-	Parameter10            string
+	Parameter10 string
 
 	// Optional. Currency code based on ISO standard. As reference - http://www.xe.com/iso4217.php
-	CurrencyCode           string
+	CurrencyCode string
 
 	// Required. Duration of call in seconds. Format 99999. Default should be 1.
-	Seconds      string
+	Seconds string
 
 	// Billing address for transaction
-	Address    Address
+	Address Address
 
 	// P2P address for transaction
 	P2PAddress P2PAddress
@@ -366,7 +366,7 @@ type ResponseWrapper struct {
 type Response struct {
 	ClientTracking string
 	HeaderMessage  string
-	ItemMessages   []string
+	ItemMessages   []ItemMessage
 	MasterTransId  int
 	ResponseCode   string
 	STAN           string
@@ -374,6 +374,12 @@ type Response struct {
 	TransId        int
 
 	GroupList []Group
+}
+
+type ItemMessage struct {
+	LineNumber   string
+	Message      string
+	ResponseCode string
 }
 
 type Group struct {
